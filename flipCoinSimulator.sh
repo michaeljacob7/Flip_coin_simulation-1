368133fd6f6c7248277aa05b8b03b891f9d43a02
michaeljacob7 <michaeljacob5770@gmail.com>
2020-10-15 10:14:17 +0530

    Added Tie condition and Winner of 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,26 +1,40 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to Flip coin Simulator program"
 
-h=0
-t=0
-
-while (( $h<11 && $t<11 ))
+hCount=0
+tCount=0
+i=1
+function tie()
+{
+while [ $hCount -le 21 ] | [ $tCount -le 21 ]
 do
-	flip=$((RANDOM%2))
-	if [ $flip -eq 1 ]
+	a=$(expr $RANDOM % 10)
+	b=$(expr $a %2)
+	echo "Coin tossed $i times"
+
+	if [ $b -eq 0 ]
 	then
-	echo "Head"
-	h=$(($h+1))
-	else
-	echo "Tail"
-	t=$(($t+1))
+		hCount=$( expr $hCount + 1 )
+	elif [ $b -eq 1 ]
+	then
+		tCount=$( expr $tCount + 1 )
+	fi
+		i=$( expr $i + 1)
+
+
+
+	if [ $hCount -eq 21 ]
+	then
+		echo "Head wins: "$hCount"times"
+	break
+	elif [ $tCount -eq 21 ]
+	then
+	echo "Tail wins: "$t"times"
+	break
 	fi
 done
 
-if [ $h -eq 10 ]
-then
-	echo "Head wins: "$h"times"
-else
-	echo "Tail wins"$t"times"
-fi
+echo "Heads won $hCount times and Tails won $tCount times"
+}
+
