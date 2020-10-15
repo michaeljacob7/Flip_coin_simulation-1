a8676e04047d612186f0da64bcf5a2ea4b1adf6a
michaeljacob7 <michaeljacob5770@gmail.com>
2020-10-15 10:30:28 +0530

    Extended previous condition till 1 side wins

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -38,3 +38,22 @@ done
 echo "Heads won $hCount times and Tails won $tCount times"
 }
 
+if [ $hCount -eq $tCount ]
+then
+        echo "Tie or toss again"
+        while [ `expr $hCount - $tCount` -lt 3 ] | [ `expr $tCount - $hCount` -lt 3 ]
+        do
+                tie $1
+        break
+        done
+fi
+
+if [ $hCount -gt $tCount ]
+then
+	d1=$( expr $hcount - $tCount )
+	echo "Heads won by $d1 times more than tails"
+else
+	d2=$( expr $tCount -$hCount )
+	echo "Tails won by $d2 times more than heads"
+fi
+
