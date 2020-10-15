5131059278baaccd6e9c1b52919a154cd1fb0af5
michaeljacob7 <michaeljacob5770@gmail.com>
2020-10-15 08:34:23 +0530

    Started flipping coins to display winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,4 +1,26 @@
+#!/bin/bash
 
 echo "Welcome to Flip coin Simulator program"
 
+h=0
+t=0
 
+while (( $h<11 && $t<11 ))
+do
+	flip=$((RANDOM%2))
+	if [ $flip -eq 1 ]
+	then
+	echo "Head"
+	h=$(($h+1))
+	else
+	echo "Tail"
+	t=$(($t+1))
+	fi
+done
+
+if [ $h -eq 10 ]
+then
+	echo "Head wins"
+else
+	echo "Tail wins"
+fi
